18f538f3fba722ab79b35cbd0762dc5c20be9669
Tanish <tanishagrawal8077@gmail.com>
2025-08-31 18:51:04 +0530

    Added PowerFor.java in Level2/ControlFlowProblems

---
+++ Level2/ControlFlowProblems/PowerFor.java
@@ -0,0 +1,17 @@
+public class PowerFor {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int number = sc.nextInt();
+        int power = sc.nextInt();
+        if (power < 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+        long result = 1;
+        for (int i = 1; i <= power; i++) {
+            result *= number;
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
