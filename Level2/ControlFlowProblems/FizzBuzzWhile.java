1d6015861b7cf88991f094170e9a99c3fccba3b0
Tanish <tanishagrawal8077@gmail.com>
2025-08-31 18:40:26 +0530

    Added FizzBuzzWhile.java in Level2/ControlFlowProblems

---
+++ Level2/ControlFlowProblems/FizzBuzzWhile.java
@@ -0,0 +1,24 @@
+public class FizzBuzzWhile {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+        int i = 1;
+        while (i <= n) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+        sc.close();
+    }
+}
