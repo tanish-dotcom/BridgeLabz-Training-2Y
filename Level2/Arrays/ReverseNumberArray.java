e7654daf613c44a4323b656029e29a7ab828e049
Tanish <tanishagrawal8077@gmail.com>
2025-09-02 20:49:15 +0530

    Added ReverseNumberArray.java program in Arrays

---
+++ Level2/Arrays/ReverseNumberArray.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ReverseNumberArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int temp = number;
+        int count = 0;
+
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+
+        int[] digits = new int[count];
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+
+        int[] reversed = new int[count];
+        for (int i = 0; i < count; i++) {
+            reversed[i] = digits[count - 1 - i];
+        }
+
+        System.out.print("Reversed Number: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(reversed[i]);
+        }
+
+        sc.close();
+    }
+}
