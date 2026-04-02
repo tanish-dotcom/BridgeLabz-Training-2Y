f2d444690f21b6c1395eeb20df7ab4f34e0c2588
Tanish <tanishagrawal8077@gmail.com>
2026-04-02 14:56:01 +0530

    Added Sliding Window Maximum problem

---
+++ Stack/SlidingWindowMaximum.java
@@ -0,0 +1,25 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlidingWindowMaximum {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return new int[0];
+        int[] result = new int[nums.length - k + 1];
+        int ri = 0;
+        Deque<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            while (!q.isEmpty() && q.peek() < i - k + 1) {
+                q.poll();
+            }
+            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+                q.pollLast();
+            }
+            q.offer(i);
+            if (i >= k - 1) {
+                result[ri++] = nums[q.peek()];
+            }
+        }
+        return result;
+    }
+}
