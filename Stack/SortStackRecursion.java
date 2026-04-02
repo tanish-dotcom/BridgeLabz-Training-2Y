75a5e4f83949df08f581d55d1badfa7be8d001f6
Tanish <tanishagrawal8077@gmail.com>
2026-04-02 14:56:04 +0530

    Added Sort Stack using Recursion

---
+++ Stack/SortStackRecursion.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class SortStackRecursion {
+    public static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int top = stack.pop();
+            sortStack(stack);
+            insertSorted(stack, top);
+        }
+    }
+
+    private static void insertSorted(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || element > stack.peek()) {
+            stack.push(element);
+            return;
+        }
+        int temp = stack.pop();
+        insertSorted(stack, element);
+        stack.push(temp);
+    }
+}
